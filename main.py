d1325d91fdc2a6d95f7b0751950c3262ee3a330d
Raghuvansh Jaitly <raghuvanshjaitly@Raghuvanshs-MacBook-Air.local>
2026-06-24 16:14:38 -0300

    Added more columnswith pandas and connected sqlite

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import yfinance as yf
 import pandas as pd
+import sqlite3
+#sql connection
+conn = sqlite3.connect('stocks.db')
+
 
 raw_data = {}
 summary = {}
@@ -9,18 +13,29 @@ tickers = ["AAPL", "MSFT", "GOOGL", "TSLA", "AMZN"]
 # store historical raw_data
 for items in tickers:
     ticker_object = yf.Ticker(items)
-    raw_data[items] = ticker_object.history(period="1y")
+    raw_data[items] = ticker_object.history(period="5y")
 
 for item in raw_data:
     
     summary[item] = {"Highest Close":raw_data[item]["Close"].max(),"Lowest Close" : raw_data[item]["Close"].min(),
-                     "Highest Volume": raw_data[item]["Volume"].max(), "Lowest Volume": raw_data[item]["Volume"].min()}
-print("Statistics")
-
+                     "Highest Volume": raw_data[item]["Volume"].max(), "Lowest Volume": raw_data[item]["Volume"].min(),
+                     "Average Close": raw_data[item]["Close"].mean(), "Average Volume":raw_data[item]["Volume"].mean(),
+                     "Median Close": raw_data[item]["Close"].median(), "Standard Deviation of Close": raw_data[item]["Close"].std()
+    }
 df = (pd.DataFrame(summary)).transpose()
-df.insert(df.index[0], "Tickers", "")
+df = df.reset_index().rename(columns={"index": "Tickers"})
+
 print(df)
 
+#write dataframe to database
+df.to_sql(
+    name='stocks_summary', #name of the table
+    con=conn, #database connection object
+    if_exists="replace", #what to do if table already exists
+    index=False #Do not write dataframe index as a seperate
+)
 
+#close connection
+conn.close()
 
 
