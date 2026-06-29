663c1e7918e6aac887abc90bcff6d2c7e7a088d2
Raghuvansh Jaitly <raghuvanshjaitly@Raghuvanshs-MacBook-Air.local>
2026-06-29 17:20:07 -0300

    Added daily price history for the last 5 years to the database

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sqlite3
 #sql connection
 conn = sqlite3.connect('stocks.db')
 
-
 raw_data = {}
+raw_data_daily = {}
 summary = {}
 tickers = ["AAPL", "MSFT", "GOOGL", "TSLA", "AMZN"]
 
@@ -14,7 +14,8 @@ tickers = ["AAPL", "MSFT", "GOOGL", "TSLA", "AMZN"]
 for items in tickers:
     ticker_object = yf.Ticker(items)
     raw_data[items] = ticker_object.history(period="5y")
-
+    raw_data_daily[items] = ticker_object.history(period="5y", interval="1d")
+    
 for item in raw_data:
     
     summary[item] = {"Highest Close":raw_data[item]["Close"].max(),"Lowest Close" : raw_data[item]["Close"].min(),
@@ -22,19 +23,27 @@ for item in raw_data:
                      "Average Close": raw_data[item]["Close"].mean(), "Average Volume":raw_data[item]["Volume"].mean(),
                      "Median Close": raw_data[item]["Close"].median(), "Standard Deviation of Close": raw_data[item]["Close"].std()
     }
-df = (pd.DataFrame(summary)).transpose()
-df = df.reset_index().rename(columns={"index": "Tickers"})
+    
+df_summary = (pd.DataFrame(summary)).transpose()
+df_summary = df_summary.reset_index().rename(columns={"index": "Tickers"})
 
-print(df)
-
+df_daily = pd.concat(raw_data_daily)
+df_daily = df_daily.reset_index().rename(columns={"level_0":"Tickers"})
+print(df_daily)
 #write dataframe to database
-df.to_sql(
+df_summary.to_sql(
     name='stocks_summary', #name of the table
     con=conn, #database connection object
     if_exists="replace", #what to do if table already exists
     index=False #Do not write dataframe index as a seperate
 )
 
+df_daily.to_sql(
+    name='daily_stock_prices',
+    con=conn,
+    if_exists="replace",
+    index=False
+)
 #close connection
 conn.close()
 
